ecdc65a7864034fe98c972bee9b3a061e8367cde
lumijoe <spacedesign63@gmail.com>
2023-10-25 09:25:25 +0900

    test gsap

--- components/Hero.js
+++ components/Hero.js
@@ -2,17 +2,29 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
 
 export default function Hero() {
+    const imageRef = useRef(null);
+
+    useEffect(() => {
+        const image = imageRef.current;
+
+        gsap.from(image, { opacity:0, duration:1, delay:1 });    
+    })
+
     return (
         <div>
-            <Image
-                src="/image01.jpg"
-                alt="Hero img"
-                width={600}
-                height={600}
-                priority
-              />
+            <div ref={imageRef}>
+                <Image
+                    src="/image01.jpg"
+                    alt="Hero img"
+                    width={600}
+                    height={600}
+                    priority
+                />
+            </div>
         </div>
     )
 }
