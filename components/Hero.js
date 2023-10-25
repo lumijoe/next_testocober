eb67a0008207f8704f5674cd1a63d0ea538b435a
lumijoe <spacedesign63@gmail.com>
2023-10-26 03:37:33 +0900

    test Hero title skills

--- components/Hero.js
+++ components/Hero.js
@@ -111,11 +111,48 @@ export default function Hero() {
             priority
           />
         </div>
-        <div className='absolute inset-0 bg-gray-950 opacity-50' />
-        <div>
-          <p className='absolute text-white text-8xl italic font-semibold origin-center rotate-90 align-baseline' style={{ top: "42vh", left: "-125px" }}>PORTFOLIO</p>
-        </div>
 
+        {/* component/Heroのoverlay */}
+        <div className='absolute inset-0 bg-gray-950 opacity-50' />
+        
+        {/* overlay上のHeroTitle */}
+        <div>
+          {/* main title PORTFOLIO */}
+          <p className='absolute text-white text-8xl italic font-semibold origin-center rotate-90 align-baseline' style={{ left: "-125px" }} >PORTFOLIO</p>
+          {/* sub title my carrrer */}
+          <div className='absolute relative' style={{ left: "25vw", lineHeight: "2px" }}>
+            <p className='text-white text-4xl italic font-semibold origin-center align-baseline' >Front&nbsp;End<span className='text-white text-2xl italic font-thin origin-center align-baseline'>Web&nbsp;Engineer</span></p>
+          </div>
+       
+        
+        {/* overlay,title,より上のskills */}
+        <div className='absolute' style={{ left: "40vw", lineHeight: "1px", bottom: "35vh" }}>
+          <p className='text-white text-2xl italic font-thin align-baseline'>
+            <ul className='text-center'>
+              <li>GSAP</li>
+              <li>HTML, CSS</li>
+              <li>WordPress</li>
+              <li>XD, Figma</li>
+              <li>Photoshop</li>
+              
+             
+            </ul>
+          </p>
+        </div>
+        <div className='absolute' style={{ left: "40vw", lineHeight: "1px", bottom: "65vh" }}>
+          <p className='text-white text-2xl italic font-thin align-baseline'>
+            <ul className='text-center'>
+              <li>NEXT.js</li>
+              <li>React</li>
+              <li>Vercel</li>
+              <li>Git, Github</li>
+              <li>JavaScript</li>
+              <li>Tailwindcss</li>
+              <li>Firebase</li>
+            </ul>
+          </p>
+        </div>
+      </div>
       </div>
     </div>
   );
