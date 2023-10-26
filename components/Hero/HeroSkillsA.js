1e579a1c51090085f142c90f17656e1e8e1dce48
lumijoe <spacedesign63@gmail.com>
2023-10-26 15:11:33 +0900

    test GSAP HeroSkillsA,B

--- components/Hero/HeroSkillsA.js
+++ components/Hero/HeroSkillsA.js
@@ -3,32 +3,30 @@
 import React, { useEffect, useRef } from 'react';
 import gsap from 'gsap';
 
-export default function  HeroTitleSkills() {
-  const textRef = useRef(null);
+export default function HeroTitleSkills() {
+    const textRef = useRef(null);
 
-  useEffect(() => {
-    const textElements = textRef.current.querySelectorAll('li');
+    useEffect(() => {
+        const textElements = gsap.utils.toArray(textRef.current.querySelectorAll('li'));
 
-    // アニメーションの設定
-    gsap.from(textElements, {
-      opacity: 0, // 最初は不透明度を0に設定
-      y: 20, // 上方向に20px移動
-      duration: 0.5,
-      stagger: 1, // 要素ごとに遅延させる
-      ease: 'power3.inOut', // イージング
-    }).reverse();
+        // アニメーションの設定
+        gsap.from(textElements.reverse(), {
+            opacity: 0, // 最初は不透明度を0に設定
+            y: 20, // 上方向に20px移動
+            duration: 0.5,
+            stagger: 1, // 要素ごとに遅延させる
+            ease: 'power3.inOut', // イージング
+        });
+        gsap.to(textElements, {
+            opacity: 1, // 最終的に不透明度を1に設定
+            y: 0, // 移動を元に戻す
+            duration: 0.1,
+            stagger: 0.5,
+            ease: 'power3.inOut',
+        });
+    }, []);
 
-    gsap.to(textElements, {
-      opacity: 1, // 最終的に不透明度を1に設定
-      y: 0, // 移動を元に戻す
-      duration: 0.1,
-      stagger: 0.5,
-      ease: 'power3.inOut',
-    }).reverse();
-
-  }, []);
-
-  return (
+    return (
     <div className='text-white text-2xl italic font-thin'>
         <ul className='text-center' ref={textRef}>
         <li>NEXT.js</li>
@@ -39,5 +37,5 @@ export default function  HeroTitleSkills() {
         <li>JavaScript</li>
         </ul>
     </div>
-  );
+    );
 };
