003d55614ac1577640d121adf2f4d3851fbb935b
lumijoe <spacedesign63@gmail.com>
2023-10-27 09:43:50 +0900

    test all

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,15 @@
 // components/Layout.js
 
 import React from 'react'
-import Header from './Header'
+import Header2 from './Header2'
 import Footer from './Footer'
+import Navpanel from './Navpanel'
 
 export default function Layout ({ children }) {
     return (
         <div>
-            <Header />
-            <main>{ children }</main>
+            <Header2 />
+            <main className='pt-[52px]'>{ children }</main>
             <Footer />
         </div>
     )
--- components/Navpanel.js
+++ components/Navpanel.js
@@ -1,17 +1,20 @@
 // components/navpanel.js
 
 import React from 'react';
+import Link from 'next/link';
 
 export default function Navpanel() {
     return (
-        <div className='bg-gray-600' style={{ width: "80vw"}}>
-             <div id="navpanel" className="navpanel ">
-                    <div className="navpanel-inner flex flex-row justify-between bg-gray-400 opacity-95 h-screen w-5/6">
-                        <h1 className='text-white'>Home</h1>
-                        <h1 className='text-white'>About</h1>
-                        <h1 className='text-white'>Works</h1>
+        <div className='bg-gray-600 h-screen' style={{ width: "80vw"}}>
+             <div id="navpanel" className="navpanel">
+                <div className="navpanel-inner bg-gray-400 opacity-50 h-screen w-5/6 ">
+                    <div className=''>
+                        <a href='/' ><p className='text-3xl text-white italic'>Home</p></a>
+                        <a href='/about' ><p className='text-3xl text-white italic'>About</p></a>
+                        <a href='/' ><p className='text-3xl text-white italic'>Works</p></a>
                     </div>
+                </div>
             </div>
         </div>
-    )
+)
 }
