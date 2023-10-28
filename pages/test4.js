0ee23201d2e7f62898d8a79710c95ee98b8e2212
lumijoe <spacedesign63@gmail.com>
2023-10-28 16:58:21 +0900

    git bug

---
+++ pages/test4.js
@@ -0,0 +1,12 @@
+// pages/test4.js
+
+import React from 'react';
+import Header2 from '../components/Header2';
+ 
+export default function Test4() {
+    return (
+        <div>
+            <Header2 />
+        </div>
+    )
+}
