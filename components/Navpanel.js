22c1ee250fb2bbd283544972ac8ec017f35a85d9
lumijoe <spacedesign63@gmail.com>
2023-11-29 23:57:46 +0900

    add text

--- components/Navpanel.js
+++ components/Navpanel.js
@@ -14,11 +14,20 @@ export default function Navpanel() {
             <div id="navpanel" className="navpanel">
                 <div className='text-white text-2xl italic font-thin pl-[3vw] pb-[50vw] pt-[calc(50vw-52px)]'>
                     <ul className='text-center'>
+                        
+                        {/* Home */}
                         <li className='text-5xl text-white italic mb-4'>
                             <Link href="/">Home</Link></li>
+                        
+                        {/* About */}
                         <li className='text-5xl text-white italic mb-4'>
                             <Link href="/test4">About</Link></li>
-                        <li className='text-5xl text-white italic mb-4'><Link href={worksarea}>Works</Link></li>
+                        
+                        {/* Works */}
+                        <li className='text-5xl text-white italic mb-4'>
+                            <Link href={worksarea}>Works</Link></li>
+                        
+                        {/* GitHub */}
                         <li className='flex justify-center mt-3'>
                             <a href="https://github.com/lumijoe" target="_blank">
                                 <Image
@@ -30,6 +39,8 @@ export default function Navpanel() {
                             </a>
                         </li>
                         <li className='text-[12px] text-white italic  -mt-[4px]'>Github</li>
+
+                        {/* Vercel */}
                         <li className='flex justify-center mt-3'>
                             <a href="https://vercel.com/lumijoes-projects" target="_blank">
                                 <Image
@@ -41,6 +52,7 @@ export default function Navpanel() {
                             </a>
                         </li>
                         <li className='text-[12px] text-white italic  -mt-[4px]'>Vercel</li>
+
                     </ul>
                 </div>
             </div>
