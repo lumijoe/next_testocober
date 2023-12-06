e47713b1d628573521b69427a0b84c94c2d3149f
lumijoe <spacedesign63@gmail.com>
2023-12-06 19:01:06 +0900

    test gif

--- pages/test3.js
+++ pages/test3.js
@@ -2,22 +2,22 @@
 
 import React from 'react';
 import Layout from '../components/Layout';
-import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Image from 'next/image';
+import video_mp4 from "."
 
 export default function Test3() {
     return (
         <div>
             <Layout>
                 <Hero />
-                <Image 
+                {/* <Image 
                 src="/work03.gif"
                 alt="Image"
                 width={395.5}
                 height={227}
                 priority
-                />
+                /> */}
                 <Image 
                 src="/7.1m_gifcompresser.gif"
                 alt="Image"
@@ -25,13 +25,14 @@ export default function Test3() {
                 height={227}
                 priority
                 />
-                <Image 
-                src="7.6m_iloveimg.gif"
+                {/* <Image 
+                src="/7.6m_iloveimg.gif"
                 alt="Image"
                 width={395.5}
                 height={227}
                 priority
-                />
+                /> */}
+                
                 <p>loremloremlorem</p>
                 <p>loremloremlorem</p>
                 <p>loremloremlorem</p>
