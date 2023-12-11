0ba28f5397e49c53d12faec9064392f9ea2812a1
lumijoe <spacedesign63@gmail.com>
2023-12-11 12:11:16 +0900

    test test5, breakPoint, title

--- components/Works07.js
+++ components/Works07.js
@@ -1,4 +1,6 @@
 // components/Works07.js
+// パターン：モバイル、左
+// ブレークポイントsm:640px
 
 import React from 'react';
 import WorksTemp from '../components/WorksTemp';
@@ -12,41 +14,46 @@ export default function Works07 () {
     return (
        <div>
             <WorksTemp>
-                <BtnLike className="bg-amber-400">2023:旅行アプリ</BtnLike>
-                <div className='flex-row mb-8'>
-                    <div className='flex'>
-                        <div className='w-[275px] pr-8'>
-                            <h2 className='font-bold text-1xl'>レジリエンス向上のために</h2>
-                            <p className='break-all mt-2 text-[12px] leading-6 text-[#696969] font-thin tracking-wider'>リハビリ通院をサポート。<br/>通院公共機関や天気情報をはじめ、主治医と共有したい症状メモ機能を搭載。
-                            <br/>アプリ１つで簡単に管理でき、リハビリに集中してもらう環境を整えるアプリです。</p>
+                <div className='flex justify-center'>
+                <BtnLike className="bg-amber-400"><span className='text-[18px] tracking-[3px]'>2023:</span>リハビリアプリ</BtnLike>
+                </div>
+
+                <div className='flex flex-col sm:flex-row'>
+                    <div className='pl-8 pr-2'> 
+                        <div className='flex flex-row mb-8'>
+                            <div className='w-[250px] pr-8'>
+                                <h2 className='font-bold text-1xl'>レジリエンス向上のために</h2>
+                                <p className='break-all mt-2 text-[12px] leading-6 text-[#696969] font-thin tracking-wider'>リハビリ通院をサポート。<br/>通院公共機関や天気情報をはじめ、主治医と共有したい症状メモ機能を搭載。アプリ１つで簡単に管理でき、リハビリに集中してもらう環境を整えるアプリです。</p>
+                            </div>
+                            <Link href='/about'>
+                                <Image
+                                    src="/toright.svg"
+                                    alt="Icon"
+                                    width={72}
+                                    height={72}
+                                    style={{ paddingTop: "25px" }}
+                                />
+                            </Link>
                         </div>
-                        <Link href='/about'>
-                            <Image
-                                src="/toright.svg"
-                                alt="Icon"
-                                width={72}
-                                height={72}
-                                style={{ paddingTop: "25px" }}
+                    </div>
+
+                    <div className='flex flex-row mr-8 h-auto'>
+                        <div className=' ml-4 overflow-visible'>
+                            <CommonImgTmb 
+                            src="/img07tmb.png"
+                            alt="Image"
+                            width={262}
+                            height={237}
+                            priority
+                            style={{ maxWidth: 'none', marginBottom: '-38px' }}
                             />
-                        </Link>
-                    </div>
-                </div>
-                <div className='flex-row'>
-                    <div className='flex'>
-                        
-                        <CommonImgTmb 
-                        src="/img07tmb.png"
-                        alt="Image"
-                        width={262}
-                        height={237}
-                        priority
-                        />
-                        <div className='flex flex-col items-center h-full'>
+                        </div>
+                        <div className='flex flex-col items-center pt-12 pr-8 z-50'>
                             <BtnLikeSkillBlack>Glide</BtnLikeSkillBlack>
-                            <BtnLikeSkillBlack>Glide Sheet</BtnLikeSkillBlack>
+                            <BtnLikeSkillBlack>GlideTables</BtnLikeSkillBlack>
                         </div>
                     </div>
-                </div>
+            </div>
             </WorksTemp>
        </div> 
     ) 
