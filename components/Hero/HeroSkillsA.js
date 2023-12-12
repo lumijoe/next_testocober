b477fbd3429c9d13de7c84a007f88f8a2a82dd13
lumijoe <spacedesign63@gmail.com>
2023-12-12 12:08:17 +0900

    staggerA,B

--- components/Hero/HeroSkillsA.js
+++ components/Hero/HeroSkillsA.js
@@ -14,14 +14,14 @@ export default function HeroTitleSkills() {
             opacity: 0, // 最初は不透明度を0に設定
             y: 20, // 上方向に20px移動
             duration: 0.5,
-            stagger: 1, // 要素ごとに遅延させる
+            stagger: 0.5, // 要素ごとに遅延させる
             ease: 'power3.inOut', // イージング
         });
         gsap.to(textElements, {
             opacity: 1, // 最終的に不透明度を1に設定
             y: 0, // 移動を元に戻す
             duration: 0.1,
-            stagger: 0.5,
+            stagger: 0.05,
             ease: 'power3.inOut',
         });
     }, []);
