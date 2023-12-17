8ea7823ffcbc0e0545b9ce643bba2cacf6e97b66
lumijoe <spacedesign63@gmail.com>
2023-12-17 20:17:46 +0900

    fix div

--- pages/web01.js
+++ pages/web01.js
@@ -8,13 +8,12 @@ import BtnLikeSKillWhite from '@/components/Button/BtnLikeSkillWhite';
 import CommonImgTmb from '@/components/Works/CommonImgTmb';
 
 export default function Web01() {
-    const URL = ''
     return (
         <div>
             <Header2 />
             <div className='relateive'>
-                <div className='bg-black w-full h-[30vh]'></div>
-                <div className='bg-gray-50 w-full h-[4vh]'></div>
+                <div className='bg-black w-full h-[30vh]'/>
+                <div className='bg-gray-50 w-full h-[4vh]'/>
                 <p className='text-white text-[12px] absolute top-[calc(56px)]'>lorem?</p>
                 <div className='absolute top-[9vh] w-full justify-center flex'>
                     <CommonImgTmb 
@@ -34,18 +33,20 @@ export default function Web01() {
                     <BtnLikeSKillWhite>Node.js</BtnLikeSKillWhite>
                 </div>
             </div>
+
             <div className='bg-[#f2f2f2] w-full h-[40vh] flex items-center'>
                 <div className='w-[80vw] '>
-                <p>ECサイト / 個人制作</p>
-                <Link href='http://design63.html.xdomain.jp/'>http://design63.html.xdomain.jp/</Link>
-                <p>ログイン認証機能 / Top / カート機能 / ブログ / 事業内容
-プライバシーポリシー / SPA / API / データベースなど
-シンプルな画像と透明感ある画像を採用し
-海やエシカルが好きな顧客のUXを高め、シンプルさにリンク
-したUIを構築しています。</p>
-</div>
-                
+                    <p>ECサイト / 個人制作</p>
+                    <Link href='http://design63.html.xdomain.jp/'>http://design63.html.xdomain.jp/</Link>
+                    <p>ログイン認証機能 / Top / カート機能 / ブログ / 事業内容
+                        プライバシーポリシー / SPA / API / データベースなど
+                        シンプルな画像と透明感ある画像を採用し
+                        海やエシカルが好きな顧客のUXを高め、シンプルさにリンク
+                        したUIを構築しています。</p>
+                </div>     
             </div>
+
+
             <p className='text-4xl'>Web01</p>
             <p className='text-4xl'>Web01</p>
             <p className='text-4xl'>Web01</p>
@@ -53,6 +54,8 @@ export default function Web01() {
             <p className='text-4xl'>Web01</p>
             <p className='text-4xl'>Web01</p>
             <p className='text-4xl'>Web01</p>
+
+
             <Footer />
         </div>
     )
