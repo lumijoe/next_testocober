ffd85e7458b1ad15ea489d19fcd00bea520b66d0
lumijoe <spacedesign63@gmail.com>
2024-01-12 01:14:09 +0900

    fix for design,code,

--- components/WorksBox.js
+++ components/WorksBox.js
@@ -9,15 +9,15 @@ import Works11 from './Works11';
 import Works08 from './Works08';
 import styles from '../styles/Footer.module.css';
 
-
-
 export default function WorksBox() {
     return (
         <div className={styles.section1}>
-            <h1 className='md:text-8xl text-6xl italic text-center pt-40 mb-20'>Design, Direction, Creative Development</h1>
-            <h2 className='text-[20px] text-center italic tracking-widest mt-60'>解決を開発するために<br/>視覚技術と言語技術を駆使する。</h2>
-            <p  id='works' className='mb-8 pt-40 text-6xl md:text-4xl italic font-semibold text-center'>Works</p>
+            {/* <h1 className='md:text-8xl text-6xl italic text-center pt-40 mb-20'>Design, Direction, Creative Development</h1> */}
+            <h1 className='md:text-8xl text-6xl italic text-center pt-40 mb-20'>Creative<br/>Development</h1>
             <span className='text-4xl thin text-center w-[6%] h-[1px] bg-pink-600 inline-block ml-[47%] mr-[47%] rotate-[-70deg] overflow-hidden'></span>
+            <h2 className='text-[20px] text-center italic tracking-widest mt-20'>解決を開発するために<br/>視覚技術と言語技術を駆使する。</h2>
+            <p  id='works' className='mb-8 pt-40 text-6xl md:text-4xl italic font-semibold text-center'>Works</p>
+            {/* <span className='text-4xl thin text-center w-[6%] h-[1px] bg-pink-600 inline-block ml-[47%] mr-[47%] rotate-[-70deg] overflow-hidden'></span> */}
             <Works02 />
             <Works01 />
             <Works10 />
