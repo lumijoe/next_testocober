0f33474bb12eb34cc92050fc18a475386486d74c
lumijoe <spacedesign63@gmail.com>
2024-01-12 23:27:58 +0900

    test

--- components/WorksTempWebSite.js
+++ components/WorksTempWebSite.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import CommonImgTmb from '../components/Works/CommonImgTmb';
+import styles from '../styles/Footer.module.css';
 
 export default function WorksTempWebSite({ categoryTitleRotate, categoryTitleNum, categoryTitleName, categoryText, children }) {
     return (
        
         <div>
             <div>
-                <h1 className=''>{categoryTitleRotate}</h1>
-                <h2 className='text-5xl align-middle'>{categoryTitleNum}
-                    <span className='text-[16px] align-middle pl-2'>{categoryTitleName}</span>
+                <h1 className={`rotate-90 ${styles.redlineRow} ml-3.5 italic `}>{categoryTitleRotate}</h1>
+                <h2 className='text-8xl align-middle italic font-semibold'>{categoryTitleNum}
+                    <span className='text-[16px] align-middle font-medium pl-2'>{categoryTitleName}</span>
                 </h2>
             </div>
             <div className='w-full justify-center'>
-                <p className='w-[50%] m-auto text-[12px] text-[#6c6c6c] tracking-wider font-extralight'>{categoryText}</p>
+                <p className='w-[50%] m-auto text-[12px] text-[#6c6c6c] tracking-wider font-300 leading-9'>{categoryText}</p>
                 {children}
                 
                 <CommonImgTmb />
