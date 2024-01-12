ba7f066469f9987d7d54e53e4d3cd4c79064e8de
lumijoe <spacedesign63@gmail.com>
2024-01-12 18:12:04 +0900

    test linered

--- components/WorksBox.js
+++ components/WorksBox.js
@@ -13,12 +13,13 @@ import styles from '../styles/Footer.module.css';
 export default function WorksBox() {
     return (
         <div className={styles.section1}>
-            {/* <h1 className='md:text-8xl text-6xl italic text-center pt-40 mb-20'>Design, Direction, Creative Development</h1> */}
             <h1 className='md:text-8xl text-6xl italic text-center pt-40 mb-20'>Creative<br/>Development</h1>
-            <span className='text-4xl thin text-center w-[6%] h-[1px] bg-pink-600 inline-block ml-[47%] mr-[47%] rotate-[-70deg] overflow-hidden'></span>
-            <h2 className='text-[20px] text-center italic tracking-widest mt-20'>解決を開発するために<br/>視覚技術と言語技術を駆使する。</h2>
-            <p  id='works' className='mb-8 pt-40 text-6xl md:text-4xl italic font-semibold text-center'>Works</p>
-            {/* <span className='text-4xl thin text-center w-[6%] h-[1px] bg-pink-600 inline-block ml-[47%] mr-[47%] rotate-[-70deg] overflow-hidden'></span> */}
+            <span className='text-4xl thin text-center w-[24px] h-[1px] bg-pink-600 inline-block ml-[47%] mr-[47%] rotate-[-67deg] overflow-hidden'></span>
+            <h2 className='text-[20px] text-center font-200 tracking-widest mt-20 leading-[40px]'>解決を開発するために<br/>視覚技術と言語技術を駆使する</h2>
+            <div className='flex flex-row mb-[210px] pt-[40px] items-center'>
+                <span className='text-4xl thin text-center w-[24px] h-[1px] bg-pink-600 inline-block rotate-[-67deg] overflow-hidden'></span>
+                <p id='works' className={`text-[40px] md:text-[60px] italic font-medium text-center`}>WORKS</p>
+            </div>
             <Works02 />
             <Works01 />
             <Works10 />
