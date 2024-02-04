3141c3e9e407a00e7db567185d501409c628de38
lumijoe <spacedesign63@gmail.com>
2024-02-04 16:14:20 +0900

    change card

--- components/Works09.js
+++ components/Works09.js
@@ -201,47 +201,6 @@ export default function Works09 () {
                         </div>
                         */}
                         
-                        
-                        {/* コーポレートサイト 
-                        ============================================================================================ */}
-                        <div className='flex flex-col justify-center relative mb-8 mr-8'>
-                            <BtnLikeY8X12 className='text-[12px] py-[7px] px-[36px] max-w-max'>コーポレートサイト</BtnLikeY8X12>
-
-                            <div className='h-auto w-[290px] bg-white' style={{ boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)', borderRadius: '4px', overflow: 'hidden', position: 'relative' }}>
-                                <CommonImgTmb 
-                                src="/workWeb01.png"
-                                alt="Image"
-                                width={280}
-                                height={156}
-                                priority
-                                style={{ maxWidth: 'none', paddingLeft: '3px', paddingTop: '30px' }}
-                                />
-                                
-                                <div className='py-7 px-[30px] '>
-                                    <p className='text-[16px] font-bold tracking-wider leading-[24px] pb-7' style={{ fontFamily: "Noto Sans JP", fontWeight: 500 }}>店舗初期のUX向上と<br/>管理課題をCMSで解決</p>
-                                    <p className='text-[12px] text-[#6c6c6c] leading-[24px]' style={{ fontFamily: "Noto Sans JP", fontWeight: 400 }}>【課題制作】企業ブランドを発信しながらイベント体験のUX向上と、ブログ管理の脱属人化を目指す。</p>
-                                    <div className='flex flex-wrap-reverse mt-[12px] mb-[5px] gap-[12px]'>
-                                    <BtnLikeSkillBlack>Photoshop</BtnLikeSkillBlack><br/>
-                                    <BtnLikeSkillBlack>WordPress</BtnLikeSkillBlack>
-                                    {/* <p className='text-[10px] text-black leading-[24px]' style={{ fontFamily: "Noto Sans JP", fontWeight: 400 }}>有限会社Ethica / 小売業</p> */}
-                                    {/* <p className='text-[12px] text-[#6c6c6c] mt-3' style={{ fontFamily: "Noto Sans JP", fontWeight: 300 }}>立ち上げフェーズでの導入により、顧客対応満足度の高いUXの提供が可能に。<br/>Direction / Design / Coding</p> */}
-                                </div>
-                                
-                                </div>
-                            </div>
-                            <Link href='http://designguys.wp.xdomain.jp/'>
-                                    <Image
-                                        src="/torightWhite.svg"
-                                        alt="Icon"
-                                        width={52}
-                                        height={52}
-                                        style={{ position: "absolute", top: "85%", left: "88%" }}
-                                    />
-                            </Link> 
-                        </div>
-                        
-                        {/* 
-                        ============================================================================================ */}
 
                     </div>
                 </div>
