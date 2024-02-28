cdf5355ab88dad0306d3cc0910d821e4de417054
lumijoe <spacedesign63@gmail.com>
2024-02-28 15:48:15 +0900

    scroll pattern one card

--- components/Works10.js
+++ components/Works10.js
@@ -17,10 +17,7 @@ export default function Works10 () {
             
         {/* bodyの上に装飾 */}
         <div className={`absolute ${styles.section1} smMin553:opacity-0 smMax552:opacity-0 smMax453:top-[660px] smMax401:top-[697px] smMax351:opacity-0  relative`}></div>
-        {/* <div className={`absolute ${styles.section1} top-[590px] smMax695:top-[605px] smMax567:top-[640px] smMax552:top-[625px] smMax453:top-[660px] smMax401:top-[697px] smMax351:opacity-0  relative`}></div> */}
-        {/* <div className='bg-[#e3e3e3] w-full h-[400px] bottom-[-35px] absolute '>
-             <div className='bg-[#e3e3e3] w-full h-[280px] absolute top-[-50px]' style={{ transform: 'skewY(-4deg)' }}></div> 
-        </div> */}
+        
         <div className='bg-[#e3e3e3] w-full h-[400px] bottom-[-35px] absolute '></div>
 
 
@@ -30,14 +27,20 @@ export default function Works10 () {
                 categoryTitleName='ランディングページ / バナー'
                 categoryText='ペルソナやデータを元に、瞬間的アプローチと継続的アプローチのバランスをコントロール。サービス体験の向上と継続を目的にPhotoshopなどで制作、コーディングも可能です。'  
             >
-                <div className='flex flex-col-reverse h-auto justify-center'>
-                    <div className='flex flex-row w-[100vw] pr-[calc(50%)] overflow-x-scroll overflow-auto 
+                <div className='flex flex-col-reverse h-auto justify-center items-center'>
+                    {/* 作品が1つの時の中央寄せ設定 */}
+                    <div className='flex flex-row w-[100vw] overflow-x-scroll overflow-auto justify-center items-center'>
+                    {/* 作品が2つ以上の時のデフォルト設定 */}
+                    {/* <div className='flex flex-row w-[100vw] pr-[calc(50%)] overflow-x-scroll overflow-auto 
                     smMax639:transform smMax639:translate-x-[50px]
-                    sm:transform sm:translate-x-[50px] '>
+                    sm:transform sm:translate-x-[50px]'> */}
 
                         
                         {/* ランディングページ表紙資料バナー */}
-                        <div className='flex flex-col justify-center relative mb-8 mr-8'>
+                        {/* 作品が1つだけの時の中央設定対応 */}
+                        <div className='flex flex-col justify-center relative mb-8'>
+                        {/* 作品が2つ以上の時のデフォルト設定対応 */}
+                        {/* <div className='flex flex-col justify-center relative mb-8 mr-8'> */}
                             <BtnLikeY8X12 className='text-[12px] py-[7px] px-[36px] max-w-max' style={{ fontFamily: "Noto Sans JP" }}>ランディングページ資料表紙</BtnLikeY8X12>
 
                             <div className='h-auto w-[290px] bg-white' style={{ boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)', borderRadius: '4px', overflow: 'hidden', position: 'relative' }}>
@@ -57,8 +60,7 @@ export default function Works10 () {
                                     <div className='flex flex-wrap-reverse mt-[12px] mb-[5px] gap-[12px]'>
                                     <BtnLikeSkillBlack>Photoshop</BtnLikeSkillBlack><br/>
                                     <BtnLikeSkillBlack>PowerPoint</BtnLikeSkillBlack>
-                                    {/* <p className='text-[10px] text-black leading-[24px]' style={{ fontFamily: "Noto Sans JP", fontWeight: 400 }}>有限会社Ethica / 小売業</p> */}
-                                    {/* <p className='text-[12px] text-[#6c6c6c] mt-3' style={{ fontFamily: "Noto Sans JP", fontWeight: 300 }}>立ち上げフェーズでの導入により、顧客対応満足度の高いUXの提供が可能に。<br/>Direction / Design / Coding</p> */}
+                                    
                                 </div>
                                 
                                 </div>
