005835ff111f8449c8c1dd5ad6317b3a94cf4ed5
lumijoe <spacedesign63@gmail.com>
2025-08-18 21:25:08 +0900

    footer copy to 2025

--- components/Footer/FooterCopy.js
+++ components/Footer/FooterCopy.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function FooterCopy () {
     return (
         <div className="pt-[40px] text-gray-200 italic text-sm text-center">
-            <p>&copy; 2023 LUMI KOJO</p>
+            <p>&copy; 2023-2025 LUMI KOJO</p>
         </div>
     )
 }
