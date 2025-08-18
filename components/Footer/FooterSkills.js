1ca904c5484542d0f3e6b0e82c2696595c48f574
lumijoe <spacedesign63@gmail.com>
2025-08-18 21:26:46 +0900

    add docker

--- components/Footer/FooterSkills.js
+++ components/Footer/FooterSkills.js
@@ -7,7 +7,7 @@ export default function FooterSkills () {
         <div className='align-middle mx-auto pt-[40px] pl-[25vw] pr-[25vw]'>
             <h1 className=' text-white italic text-[16px] tracking-widest'>Skills</h1>
             <p className='text-white italic text-[16px] leading-6 tracking-widest'>React.js&nbsp; /&nbsp; Next.js &nbsp;/&nbsp; Node.js &nbsp;/&nbsp; GSAP &nbsp;/&nbsp; Tailwindcss &nbsp;/&nbsp; Bootstrap &nbsp;/
-Git, Github  /&nbsp; FLOCSS &nbsp;/&nbsp; Sass &nbsp;/&nbsp; Sourcetree &nbsp;/&nbsp; Bitbucket &nbsp;/  Vercel  /  Firebase  /  Express  / Vue.js / JavaScript  /  HTML, CSS  /  Bootstrap  / VSCode  /  WordPress  /  Photoshop /  Illustrator /  XD  / Figma / Coding  /  Design  /  Planning  /  Direction / Branding</p>
+Git, Github  /&nbsp; FLOCSS &nbsp;/&nbsp; Sass &nbsp;/&nbsp; Sourcetree &nbsp;/&nbsp; Bitbucket &nbsp;/  Vercel  /  Firebase  /  Express  / Vue.js / JavaScript  /  HTML, CSS  /  Bootstrap  / VSCode  /  WordPress  /  Docker  /  Photoshop  /  XD  / Figma / Coding  /  Design  /  Planning  /  Direction / Branding</p>
         </div>
     )
 }
