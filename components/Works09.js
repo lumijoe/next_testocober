ffb919ff2bae1c182fbeb51b0710db6b895928d2
lumijoe <spacedesign63@gmail.com>
2025-08-28 08:55:14 +0900

    add test

--- components/Works09.js
+++ components/Works09.js
@@ -114,6 +114,42 @@ export default function Works09 () {
                                 </div>
                             </Link>
                         </div>
+
+                        {/* ポートフォリオサイト 
+                        ============================================================================================ */}
+                        <div className='flex flex-col justify-center relative mb-8 mr-8 bg-opacity-100'>
+                            <BtnLikeY8X12 className='text-[12px] py-[7px] px-[36px] max-w-max'>
+                                現ポートフォリオサイト
+                            </BtnLikeY8X12>
+                            <Link href='/'>
+                                <div className='h-auto w-[400px]'>
+                                    <CommonImgTmb 
+                                    src="/310264_3.png"
+                                    alt="Image"
+                                    width={400}
+                                    height={264}
+                                    priority
+                                    style={{ maxWidth: 'none' }}
+                                    />
+                                    <div className='py-5 px-[30px] h-[320px] bg-white' style={{ boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)', borderRadius: '0px 0px 4px 4px', overflow: 'hidden', position: 'relative' }} >
+                                        <p className='text-[18px] font-bold tracking-wider leading-[36px] pb-5' style={{ fontFamily: "Noto Sans JP", fontWeight: 500 }}>
+                                            個人制作<br/>Next.jsをVercelでデプロイ
+                                        </p>                                    
+                                        <p className='text-[14px] text-[#6c6c6c] leading-[24px] pb-5' style={{ fontFamily: "Noto Sans JP", fontWeight: 300 }}>
+                                            2023/10-<br/>Web design / Branding design / Planning / Coding / Deploy
+                                        </p>
+                                        <div className='flex flex-wrap-reverse mt-[12px] mb-[5px] gap-[12px]'>
+                                            <BtnLikeSkillBlack>XD</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Next.js</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Photoshop</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Vercel</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>GSAP</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Tailwindcss</BtnLikeSkillBlack>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
                         {/* ポートフォリオサイト 
                         ============================================================================================ */}
                         <div className='flex flex-col justify-center relative mb-8 mr-8 bg-opacity-100'>
