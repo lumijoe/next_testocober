977058f9b93f1e9327c268c1cca1dce473b5530a
lumijoe <spacedesign63@gmail.com>
2025-08-28 09:35:35 +0900

    add ind1221

--- components/Works09.js
+++ components/Works09.js
@@ -119,12 +119,12 @@ export default function Works09 () {
                         ============================================================================================ */}
                         <div className='flex flex-col justify-center relative mb-8 mr-8 bg-opacity-100'>
                             <BtnLikeY8X12 className='text-[12px] py-[7px] px-[36px] max-w-max'>
-                                現ポートフォリオサイト
+                                製造業コーポレートサイト
                             </BtnLikeY8X12>
-                            <Link href='/'>
+                            <Link href='https://lkcodetest.sakura.ne.jp/ind1221/'>
                                 <div className='h-auto w-[400px]'>
                                     <CommonImgTmb 
-                                    src="/310264_3.png"
+                                    src="/310264_4.png"
                                     alt="Image"
                                     width={400}
                                     height={264}
@@ -133,18 +133,18 @@ export default function Works09 () {
                                     />
                                     <div className='py-5 px-[30px] h-[320px] bg-white' style={{ boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.1)', borderRadius: '0px 0px 4px 4px', overflow: 'hidden', position: 'relative' }} >
                                         <p className='text-[18px] font-bold tracking-wider leading-[36px] pb-5' style={{ fontFamily: "Noto Sans JP", fontWeight: 500 }}>
-                                            個人制作<br/>Next.jsをVercelでデプロイ
+                                            業務モックアップ制作<br/>Word Pressカスタムテーマ構築
                                         </p>                                    
                                         <p className='text-[14px] text-[#6c6c6c] leading-[24px] pb-5' style={{ fontFamily: "Noto Sans JP", fontWeight: 300 }}>
-                                            2023/10-<br/>Web design / Branding design / Planning / Coding / Deploy
+                                            2024-実装中<br/>Web design / Branding design / Planning / Coding / Deploy
                                         </p>
                                         <div className='flex flex-wrap-reverse mt-[12px] mb-[5px] gap-[12px]'>
-                                            <BtnLikeSkillBlack>XD</BtnLikeSkillBlack>
-                                            <BtnLikeSkillBlack>Next.js</BtnLikeSkillBlack>
-                                            <BtnLikeSkillBlack>Photoshop</BtnLikeSkillBlack>
-                                            <BtnLikeSkillBlack>Vercel</BtnLikeSkillBlack>
-                                            <BtnLikeSkillBlack>GSAP</BtnLikeSkillBlack>
-                                            <BtnLikeSkillBlack>Tailwindcss</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Figma</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Word Press(PHP)</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>プラグイン</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>Bootstrap</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>多言語対応</BtnLikeSkillBlack>
+                                            <BtnLikeSkillBlack>OR検索機能</BtnLikeSkillBlack>
                                         </div>
                                     </div>
                                 </div>
